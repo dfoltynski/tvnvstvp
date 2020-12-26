e6402cba53de943776fa721dc8e8994777ea56ca
dfoltynski <dawidfoltynski7@gmail.com>
2020-12-26 18:00:55 +0100

    backend: get url to article

--- backend/server.ts
+++ backend/server.ts
@@ -30,8 +30,27 @@ app.get(
                 }
             );
 
+            let topStoryURL = await page.$$eval(
+                ".top-story-container",
+                (url) => {
+                    return url.map((u) =>
+                        u.children[0].children[1].children[0].children[0].getAttribute(
+                            "href"
+                        )
+                    );
+                }
+            );
+
             await page.evaluate(() => {
                 window.scrollTo(0, window.scrollY);
+            });
+
+            const urls = await page.$$eval(".teaser-wrapper", (teaser) => {
+                return teaser.map((t) =>
+                    t.children[0].children[0].children[0].children[0].getAttribute(
+                        "href"
+                    )
+                );
             });
 
             // // Scroll to the bottom of the page with puppeteer-autoscroll-down
@@ -48,9 +67,9 @@ app.get(
             const newsFromTVN = new Map();
             headings.forEach((header, index) => {
                 if (index === 0) {
-                    newsFromTVN.set(header, topStoryImage);
+                    newsFromTVN.set(header, [topStoryImage, topStoryURL]);
                 } else if (images[index] && index < 10) {
-                    newsFromTVN.set(header, images[index]);
+                    newsFromTVN.set(header, [images[index], urls[index]]);
                 }
             });
             return newsFromTVN;
@@ -73,14 +92,23 @@ app.get(
             );
 
             let images = await page.$$eval(".img-responsive", (imgSrc) => {
-                return imgSrc.map((i) => i.getAttribute("src"));
+                return imgSrc.map(
+                    (i) => "https://www.tvp.info/" + i.getAttribute("src")
+                );
+            });
+
+            let urls = await page.$$eval(".main-mesh-box__image", (url) => {
+                return url.map((u) => u.getAttribute("href"));
             });
 
             browser.close();
             const newsFromTVP = new Map();
             headings.forEach((header, index) => {
                 if (images[index] && index < 10) {
-                    newsFromTVP.set(header, images[index + 1]);
+                    newsFromTVP.set(header, [
+                        images[index + 1],
+                        urls[index + 1],
+                    ]);
                 }
             });
 
